629ca43791d961dca9498382ebe82e8a62fbfe7c
Trevor Fehrman <trevorfehrman@gmail.com>
2024-04-06 15:58:52 -0700

    remove unused packages

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ import createJiti from "jiti";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti("./env.ts");
 
-// import withPlaiceholder from "@plaiceholder/next";
-
 const nextConfig = {
   images: {
     remotePatterns: [
