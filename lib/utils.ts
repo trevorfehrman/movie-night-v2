c8aaeb9768b1a2e904d13085ee98d7e336159368
Trevor Fehrman <trevorfehrman@gmail.com>
2024-04-14 17:04:58 -0700

    corrected import path

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { getMovieDetails } from "./tmdb/movies";
+import { getMovieDetails } from "./tmdb/get-movie-details";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
