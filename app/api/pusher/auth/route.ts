6c4c6f07224735aba3b6cd1d2dae098b50e9c06a
Trevor Fehrman <trevorfehrman@gmail.com>
2024-05-03 16:52:46 -0500

    return unhappy path response from auth route for pusher

--- app/api/pusher/auth/route.ts
+++ app/api/pusher/auth/route.ts
@@ -4,18 +4,14 @@ import { auth } from "@clerk/nextjs/server";
 const pusherServer = getPusherInstance();
 
 export async function POST(req: Request) {
-  console.log("authenticating pusher perms...");
   const { userId: clerkUserId, orgId, has } = auth();
   if (!clerkUserId || !orgId || !has({ permission: "org:movie:create" })) {
-    console.log("no clerkUserId, orgId, or userFirstName");
-    return;
+    return new Response("Unauthorized", { status: 401 });
   }
   const data = await req.text();
   const [socketId, channelName] = data
     .split("&")
     .map((str) => str.split("=")[1]);
-
-  // logic to check user permissions
 
   const authResponse = pusherServer.authorizeChannel(socketId, channelName);
 
